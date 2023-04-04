da886f182d5e69335f62ee49d52a949b0ae6ea58
Killian PERRICHET <perrichetkillian@gmail.com>
2023-04-04 21:57:28 +0200

    Optimisation

--- vue/header.php
+++ vue/header.php
@@ -1,18 +1,33 @@
-<body>
-      <header class="container-header">
-        <div>
-          <p>
-          <h1 class="txt-al-center NeonEffect">PERRICHET KILLIAN</h1>
-          </p>
-          <p>
-          <h1 class="txt-al-center NeonEffect">DEVELOPPEUR WEB</h1>
-          </p>
-        </div>
-      </header>
-      <nav class="container-nav">
-        <a href="./?action=home" class="m-16 NeonButton">Accueil</a>
-        <a href="./?action=etude" class="m-16 NeonButton">Étude</a>
-        <a href="./?action=projet" class="m-16 NeonButton">Projet</a>
-        <a href="./?action=competences" class="m-16 NeonButton">Compétences</a>
-      </nav>
-</body>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PORTFOLIO PERRICHET</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"/>
+    <link rel="stylesheet" href="./src/css/swiper-bundle.min.css">
+    <link rel="stylesheet" href="./src/css/master.css">
+    <link rel="stylesheet" href="./src/css/timeline.css">
+    <link rel="stylesheet" href="./src/css/projet.css" class="src">
+    <link rel="stylesheet" href="./src/css/slider.css">
+  </head>
+  <body>
+        <header class="container-header">
+          <div>
+            <p>
+            <h1 class="txt-al-center NeonEffect">PERRICHET KILLIAN</h1>
+            </p>
+            <p>
+            <h1 class="txt-al-center NeonEffect">DEVELOPPEUR WEB</h1>
+            </p>
+          </div>
+        </header>
+        <nav class="container-nav">
+          <a href="./?action=home" class="m-16 NeonButton">Accueil</a>
+          <a href="./?action=etude" class="m-16 NeonButton">Étude</a>
+          <a href="./?action=projet" class="m-16 NeonButton">Projet</a>
+          <a href="./?action=competences" class="m-16 NeonButton">Compétences</a>
+        </nav>
+  </body>
+</html>
