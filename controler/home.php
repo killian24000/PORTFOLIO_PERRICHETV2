0466378610af0b5adba1ca7f5219276c76e0d412
Killian PERRICHET <perrichetkillian@gmail.com>
2023-04-17 21:33:35 +0200

    Update home.php

--- controler/home.php
+++ controler/home.php
@@ -11,7 +11,7 @@ if(isset($_POST['mailform'])){
   $message = $_POST['message'];
 
   $header="MIME-Version: 1.0\r\n";
-  $header.='From:"'.$name.'' .$prenom.'"<'.$email.'>'."\n";
+  $header.='From:"'.$name. '' .$prenom.'"<'.$email.'>'."\n";
   $header.='Content-Type:text/html; charset="uft-8"'."\n";
   $header.='Content-Transfer-Encoding: 8bit';
   
