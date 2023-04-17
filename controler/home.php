99141ebfd8c247cd54dc028ca16b6842f97acf18
Killian PERRICHET <perrichetkillian@gmail.com>
2023-04-17 21:17:05 +0200

    update mail

--- controler/home.php
+++ controler/home.php
@@ -3,6 +3,33 @@ if( $_SERVER["SCRIPT_FILENAME"] == __FILE__){
   $racine="..";
 }
 
+if(isset($_POST['mailform'])){
+
+  $email = $_POST['email'];
+  $name = $_POST['name'];
+  $prenom = $_POST['prenom'];
+  $message = $_POST['message'];
+
+  $header="MIME-Version: 1.0\r\n";
+  $header.='From:"'.$name.''.$prenom.'"<'.$email.'>'."\n";
+  $header.='Content-Type:text/html; charset="uft-8"'."\n";
+  $header.='Content-Transfer-Encoding: 8bit';
+  
+  $message='
+  <html>
+      <body>
+          <div align="center">
+              '.$message.'
+          </div>
+      </body>
+  </html>
+  ';
+
+  //echo($message);
+  
+  mail("killian.perrichet@androcode.fr", "Salut tout le monde !", $message, $header);
+}
+
 $titre = "Liste des formations";
 include "$racine/vue/header.php";
 include "$racine/vue/home.phtml";
