a3de926e582e53298c034c89ad4a76cf9f4c9092
Killian PERRICHET <perrichetkillian@gmail.com>
2023-04-22 22:56:50 +0200

    Update logo

--- vue/header.php
+++ vue/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./src/css/K.png">
     <title>PORTFOLIO / PERRICHET</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"/>
     <link rel="stylesheet" href="./src/css/swiper-bundle.min.css">
